93f59584852de8cc627b9f8847ecfdfef4765c38
lawrenceyim <lawrence.yim.010@gmail.com>
2023-07-19 23:58:18 -0400

    Added barrier and changed camera script

--- Assets/Script/CameraMovement.cs
+++ Assets/Script/CameraMovement.cs
@@ -6,6 +6,11 @@ public class CameraMovement : MonoBehaviour
 {
     private GameObject player;
     [SerializeField] private float z;
+    [SerializeField] private float topLimit;
+    [SerializeField] private float bottomLimit;
+    [SerializeField] private float leftLimit;
+    [SerializeField] private float rightLimit;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +21,17 @@ public class CameraMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, z);
+        Vector3 position = player.transform.position;
+        if (player.transform.position.x > rightLimit) {
+            position.x = rightLimit;
+        } else if (player.transform.position.x < leftLimit) {
+            position.x = leftLimit;
+        } 
+        if (player.transform.position.y > topLimit) {
+            position.y = topLimit;
+        } else if (player.transform.position.y < bottomLimit) {
+            position.y = bottomLimit;
+        }
+        transform.position = new Vector3(position.x, position.y, z);
     }
 }
