a4fde8f096a258c3449581f9e2c83f9c827838ba
lawrenceyim <lawrence.yim.010@gmail.com>
2023-07-20 20:39:36 -0400

    Added code to stop players from playing locked levels

--- Assets/Script/Level Menu/LevelStarter.cs
+++ Assets/Script/Level Menu/LevelStarter.cs
@@ -6,8 +6,16 @@ using TMPro;
 
 public class LevelStarter : MonoBehaviour
 {
+    [SerializeField] private bool levelAvailable;
     private void OnMouseDown() {
+        if (!levelAvailable) {
+            return;
+        }
         string sceneName = "Level " + GetComponent<TextMeshPro>().text;
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void MakeLevelAvailable() {
+        levelAvailable = true;
     }
 }
