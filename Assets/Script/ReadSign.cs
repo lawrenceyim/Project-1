3f81a9dd9f9ef26ed9a0d3d00c346020147da798
lawrenceyim <lawrence.yim.010@gmail.com>
2023-07-24 00:11:56 -0400

    Added background to signs

--- Assets/Script/ReadSign.cs
+++ Assets/Script/ReadSign.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class ReadSign : MonoBehaviour
 {
     [SerializeField] GameObject textObject;
-    
+    [SerializeField] GameObject background;    
     private void Start() {
         textObject.GetComponent<MeshRenderer>().enabled = false;
+        background.GetComponent<SpriteRenderer>().enabled = false;
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.CompareTag("Player")) {
             textObject.GetComponent<MeshRenderer>().enabled = true;
+            background.GetComponent<SpriteRenderer>().enabled = true;
         }
     }
 
         private void OnTriggerExit2D(Collider2D other) {
         if (other.CompareTag("Player")) {
             textObject.GetComponent<MeshRenderer>().enabled = false;
+            background.GetComponent<SpriteRenderer>().enabled = false;
         }
     }
 }
